edc4e7fe023c34a90a45130ee3e48dcd66615a59
android-rooted <50019150+android-rooted@users.noreply.github.com>
2019-08-28 13:52:44 +0530

    Add files via upload

---
+++ .about.sh
@@ -0,0 +1,63 @@
+clear
+clear
+clear
+a="$(tput setaf 10)"
+b="$(tput setaf 11)"
+c="$(tput setaf 77)"
+d="$(tput setaf 13)"
+e="$(tput setaf 14)"
+f="$(tput setaf 75)"
+g="$(tput setaf 68)"
+h="$(tput setaf 9)"
+i="$(tput setaf 32)"
+j="$(tput setaf 33)"
+k="$(tput setaf 41)"
+l="$(tput setaf 40)"
+m="$(tput setaf 42)"
+n="$(tput setaf 45)"
+o="$(tput setaf 46)"
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+toilet -f term -F gay =============================================
+echo "$a Termux-New-Look $b Create $c by $d Maruf"
+toilet -f term -F gay =============================================
+echo
+echo
+echo "$e Many Feature Available Termux-New-Look"
+echo "$i Termux-New-Look specially for new Termux user"
+echo
+echo "$a Termux-New-Look Last Updated$b  28 August 2019"
+echo 
+echo "$c My github link$d :-$e http://github.com/android-rooted"
+echo
+echo "$d This Tool Github Link$f :-$g http://github.com/android-rooted/Termux-New-Look"
+echo
+echo
+toilet -f term -F gay =============================================
+echo "	$a Termux-New-Look $b About $c End"
+toilet -f term -F gay =============================================
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
