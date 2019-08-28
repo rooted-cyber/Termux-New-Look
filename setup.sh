f977cce08ffca635603f15c5c1ed701c1a71e55f
android-rooted <50019150+android-rooted@users.noreply.github.com>
2019-08-28 13:51:35 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -25,6 +25,8 @@ cp -f .bashrc $HOME
 cp -f .Termux_os.sh $HOME
 cp -f .Open.sh $HOME
 cp -f .short.sh $HOME
+cp -f .about.sh $HOME
+chmod 777 $HOME/.about.sh
 chmod 777 $HOME/.short.sh
 chmod 777 $HOME/.Installation.sh
 chmod 777 $HOME/.Termux_os.sh
