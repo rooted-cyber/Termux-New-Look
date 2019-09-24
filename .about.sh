e11303a234fece0888db4a098ded538d0344e07a
rooted-cyber <50019150+rooted-cyber@users.noreply.github.com>
2019-09-24 15:12:57 +0530

    Add files via upload

---
+++ .about.sh
@@ -0,0 +1,91 @@
+clear
+clear
+clear
+a="$(tput setaf 10)"
+b="$(tput setaf 11)"
+c="$(tput setaf 77)"
+d="$(tput setaf 13)"
+e="$(tput setaf 14)"
+f="$(tput setaf 75)"
+g="$(tput setaf 68)"
+h="$(tput setaf 9)"
+i="$(tput setaf 32)"
+j="$(tput setaf 33)"
+k="$(tput setaf 41)"
+l="$(tput setaf 40)"
+m="$(tput setaf 42)"
+n="$(tput setaf 45)"
+o="$(tput setaf 46)"
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+toilet -f term -F gay =============================================
+echo "$a Termux-New-Look $b Create $c by $d Maruf"
+toilet -f term -F gay =============================================
+echo
+echo
+echo "$a Many Feature Available Termux-New-Look"
+echo "$a Termux-New-Look specially for new Termux user"
+echo
+echo "$a Termux-New-Look Last Updated$b  28 August 2019"
+echo 
+echo "$a My github link$d :-$e http://github.com/android-rooted"
+echo
+echo "$a This Tool Github Link$f :-$e http://github.com/android-rooted/Termux-New-Look"
+echo
+echo -e "\033[92m 		Termux-New-Look Changelog:-"
+echo
+echo -e "\033[93m Termux-New-Look version 2.4"
+echo
+echo -e "\033[96m"
+echo -e " [+] Added New Shell"
+echo -e " [+] Added Checking version"
+echo
+echo -e "\033[93m Termux-New-Look version 2.3"
+echo
+echo -e "\033[96m"
+echo -e " [~] Termux New Look Ui changed"
+echo -e " [~] Termux-Os Ui changed"
+echo -e " [~] Added some tools in installation"
+echo -e " [~] Some things Removed"
+echo -e " [~] Some bugs fixed"
+echo
+echo -e "\033[93m Termux-New-Look version 2.2"
+echo
+echo -e "\033[96m"
+echo " [~] Added shortcut command"
+echo " [~] Some bugs fixes"
+echo
+echo -e "\033[93mTermux-New-Look version 2.1"
+echo
+echo -e "\033[96m"
+echo " [~] Change Design Termux-New-Look"
+echo " [~] Change color of Termux Boot Menu"
+echo " [~] Updated Installation Menu"
+echo " [~] Some bugs fixed"
+echo
+echo -e "\033[93m Termux-New-Look version 2.0"
+echo
+echo -e "\033[96m"
+echo " [~] Added Open Termux"
+echo " [~] Added Termux root"
+echo " [~] Added Pip Upgrade"
+echo " [~] Added Installation Menu"
+echo " [~] Added Open tools"
+echo " [~] Added Termux os"
+echo " [~] Added Termux Game"
+echo " [~] Added Some shortcut command"
+echo " [~] Added Update Termux-New-Look"
+echo " [~] Added Uninstall Termux-New-Look"
+echo
+toilet -f term -F gay =============================================
+echo "	$a Termux-New-Look $b About $c End"
+toilet -f term -F gay =============================================
+echo
+echo
