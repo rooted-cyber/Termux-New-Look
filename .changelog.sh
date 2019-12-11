dfd69590bd4ddc30f21e10d783c066af4bf3290d
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2019-12-11 09:16:37 +0530

    Add files via upload

--- .changelog.sh
+++ .changelog.sh
@@ -28,7 +28,7 @@ echo "$a My github link$d :-$e http://github.com/rooted-cyber"
 echo
 printf "$a This Tool Github Link$f \n\n$e http://github.com/rooted-cyber/Termux-New-Look\n\n"
 echo -e "\033[92m 		Termux-New-Look Changelog:-"
-printf "\033[93m\n Termux-New-Look 3.5\n"
+printf "\033[93m\n Termux-New-Look 3.6\n"
 echo -e "\033[96m"
 printf " [+] Fix Termux-Game\n"
 printf " [+] Added Open-Tools\n"
