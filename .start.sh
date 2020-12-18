904ab481177ff861d0fdad2d5c16086ef9872b8f
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2020-12-18 20:57:58 +0530

    Add files via upload

--- .start.sh
+++ .start.sh
@@ -3,22 +3,10 @@ printf "===========================================\n"
 printf "		\033[96m Termux-New-Look\033[0m\n"
 printf "===========================================\n\n"
 printf "\033[96m ● Feature :- \n"
-printf "\033[92m"
+printf "\033[1;92m"
 printf "
- ● Added New Shell.
- ● Added Tools Menu.
- ● Added Termux-Common Menu.
- ● Added Metasploit installation menu.
- ● Added Metasploit menu.
- ● Added Virus Maker in menu.
- ● Added Messanger menu.
- ● Added Termux-Other feature in menu.
- ● Added TI script in menu.
- ● Added Termux extra button in menu.
- ● Added Termux-Apktool in menu.
- • Added Downloading ngrok.
- ● Added shortcut command to install & open tools.\n\n\033[0m"
- 
+ • Added menu command to see menu
+ • Added many shortcut
 printf "===========================================\n"
 printf "		\033[93m Termux-New-Look\033[0m\n"
 printf "===========================================\n\n"
